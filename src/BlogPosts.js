2038ea91d06d71bc7b516ed7c24233aa31beff4e
Kaitlyn Greve <kgreve14@gmail.com>
2023-01-25 13:13:11 -0800

    adding data for projects and blog posts

--- src/BlogPosts.js
+++ src/BlogPosts.js
@@ -1,15 +1,5 @@
 import BlogPostCard from './BlogPostCard'
 
-const blogPosts = [
-    {
-        title: 'CSS Grid',
-        blurb: '...'
-    },
-    {
-        title: 'useNavigate',
-        blurb: '...'
-    }
-]
 
 function BlogPosts() {
     return (
@@ -22,3 +12,30 @@ function BlogPosts() {
 }
 
 export default BlogPosts;
+
+const blogPosts = [
+    {
+        title: 'CSS Grid - Step by Step',
+        blurb: 'Whether you are new to CSS grid or simply need a refresher with some easy to follow steps — you have clicked on the right post! My hope for you is that by the end of reading this post you have confidence in implementing a CSS grid in your project...',
+        blog_link: 'https://medium.com/towardsdev/css-grid-step-by-step-aad05ec91f4b',
+        img: ''
+    },
+    {
+        title: 'useHistory => useNavigate',
+        blurb: 'With ReactRouter updating from version 5 to version 6, there have been a few changes. One of which is the replacement of the useHistory() hook to useNavigate()...',
+        blog_link: 'https://medium.com/@kgreve14/usehistory-usenavigate-5b383160adba',
+        img: ''
+    },
+    {
+        title: 'CSS Flexbox',
+        blurb: 'CSS can be intimidating. Not because it is overly complicated or causes errors, (actually … part of why CSS can be tricky is because it does not cause errors 😅), but because it can do so many things!...',
+        blog_link: 'https://medium.com/@kgreve14/css-flexbox-28d11dc42891',
+        img: ''
+    },
+    {
+        title: 'Knowing Linked Lists for JavaScript Developers',
+        blurd: 'Considered a “core” data structure, linked lists may seem a little cumbersome to a JavaScript developer...',
+        blog_link: 'https://medium.com/towardsdev/knowing-linked-lists-for-javascript-developers-87adfdb87838',
+        img: ''
+    }
+]
--- src/Projects.js
+++ src/Projects.js
@@ -1,15 +1,5 @@
 import Projectcard from './ProjectCard';
 
-const projects = [
-    {
-        name: 'Notable',
-        description: 'For note taking'
-    },
-    {
-        name: 'Recipy',
-        description: 'Digital recipe box'
-    }
-];
 
 function Projects() {
     return (
@@ -22,3 +12,30 @@ function Projects() {
 }
 
 export default Projects;
+
+const projects = [
+    {
+        name: 'Notable',
+        img: '',
+        description: 'A web app built for taking and keeping notes. Notable\s neat design makes navigating and organization simple. Create and save your note entries, and revisit them, with the ability to add to or delete your notes.',
+        tech_stack: 'React, CSS, Firebase',
+        github_url: 'https://github.com/kaitlyngreve/journal-app',
+        deployed_url: 'https://journal-app-42603.web.app/'
+    },
+    {
+        name: 'Recipy',
+        img: '',
+        description: 'A digital recipe box for storing and creating recipes. Born from a love of being in the kitchen, Recipy (rec-i-pe) encourages trying new recipes of either your own creation or others.',
+        tech_stack: 'React, Ruby on Rails, CSS',
+        github_url: 'https://github.com/kaitlyngreve/digital-recipebox',
+        deployed_url: ''
+    },
+    {
+        name: 'Taskier',
+        img: '',
+        description: 'A single page web app for keeping a simple task list.',
+        tech_stack: 'React, CSS',
+        github_url: 'https://github.com/kaitlyngreve/taskier-a-todo-react-app',
+        deployed_url: ''
+    }
+];
