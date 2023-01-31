cef08cfc0f4cb07731b703db30e354736dc4e612
Kaitlyn Greve <kgreve14@gmail.com>
2023-01-31 10:53:15 -0800

    implemented collabsible divs

--- src/About.js
+++ src/About.js
@@ -2,29 +2,31 @@
 
 function About() {
     return (
-        <div id='about'>
-            <h1>About me</h1>
-            <p>Hello! My name is Kaitlyn and I am a full stack software engineer based in the Bay Area, California.</p>
-            <p>I graduated from and intensive full stack software engineering bootcamp, but before
-                that I worked as a hairstylist in the cosmetology industry. I've loved that I can
-                apply my passion for creating and delivering meaningful results to people from cosmetology
-                to coding. Part of what drew me to tech was wanting to make a bigger impact - whether it be through
-                improving workflows and collaboration, bettering how tools and platforms work together,
-                or data security. I admire how software is so widely used and provides ample opportunity
-                to reach and impact groups of people.
-            </p>
-            <p>As a software engineer I can apply my excitment for continued learning, teamwork, and innovation
-                on a team that is equally excited for these things. I am eager to use my skills to build
-                and contribute toward software that empowers people and organizations.
-            </p>
-            <p>Here are a few technologies I have been working with:</p>
-            <ul>
-                <li>JavaScript</li>
-                <li>React</li>
-                <li>Firebase</li>
-                <li>Ruby</li>
-                <li>Ruby on Rails</li>
-            </ul>
+        <div id="about">
+            <div className="about_me">
+                <h1>About me</h1>
+                <p>Hello! My name is Kaitlyn and I am a full stack software engineer based in the Bay Area, California.</p>
+                <p>I graduated from and intensive full stack software engineering bootcamp, but before
+                    that I worked as a hairstylist in the cosmetology industry. I've loved that I can
+                    apply my passion for creating and delivering meaningful results to people from cosmetology
+                    to coding. Part of what drew me to tech was wanting to make a bigger impact - whether it be through
+                    improving workflows and collaboration, bettering how tools and platforms work together,
+                    or data security. I admire how software is so widely used and provides ample opportunity
+                    to reach and impact groups of people.
+                </p>
+                <p>As a software engineer I can apply my excitment for continued learning, teamwork, and innovation
+                    on a team that is equally excited for these things. I am eager to use my skills to build
+                    and contribute toward software that empowers people and organizations.
+                </p>
+                <p>Here are a few technologies I have been working with:</p>
+                <ul>
+                    <li>JavaScript</li>
+                    <li>React</li>
+                    <li>Firebase</li>
+                    <li>Ruby</li>
+                    <li>Ruby on Rails</li>
+                </ul>
+            </div>
         </div>
     )
 }
--- src/ProjectCard.js
+++ src/ProjectCard.js
@@ -1,10 +1,26 @@
-
+import useCollapse from "react-collapsed";
+import { useState } from 'react';
 
 function ProjectCard({ project }) {
+    const [isExpanded, setIsExpanded] = useState(false);
+    const { getCollapseProps, getToggleProps } = useCollapse({ isExpanded });
+
+    const handleCollapse = () => {
+        setIsExpanded(!isExpanded)
+    }
+
     return (
-        <div ide='projects'>
-            <p>{project.name}</p>
+        <div>
+            <div id='projects'></div>
+            <div className='collapsible-div' {...getToggleProps({ onClick: handleCollapse })}>
+                {isExpanded ? project.name : project.name}
+            </div>
+            <div className='collapsible-div' {...getCollapseProps()}>
+                <p>{project.description}</p>
+                <p>{project.tech_stack}</p>
+            </div>
         </div>
+
     )
 }
 
