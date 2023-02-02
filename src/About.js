f4c618458866110a525c393ad88400c4865b417d
Kaitlyn Greve <kgreve14@gmail.com>
2023-02-01 17:12:15 -0800

    final push for today

--- src/About.js
+++ src/About.js
@@ -2,12 +2,12 @@
 
 function About() {
     return (
-        <div>
+        <div className="about-container">
             <div id="about"></div>
-            <div className="about_me">
-                <h1>About me</h1>
-                <p>Hello! My name is Kaitlyn and I am a full stack software engineer based in the Bay Area, California.</p>
-                <p>I graduated from and intensive full stack software engineering bootcamp, but before
+            <div className="about-me">
+                <h1 className="about-header">About me</h1>
+                <p className="about-content">Hello! My name is Kaitlyn and I am a full stack software engineer based in the Bay Area, California.</p>
+                <p className="about-content">I graduated from and intensive full stack software engineering bootcamp, but before
                     that I worked as a hairstylist in the cosmetology industry. I've loved that I can
                     apply my passion for creating and delivering meaningful results to people from cosmetology
                     to coding. Part of what drew me to tech was wanting to make a bigger impact - whether it be through
@@ -15,13 +15,13 @@ function About() {
                     or data security. I admire how software is so widely used and provides ample opportunity
                     to reach and impact groups of people.
                 </p>
-                <p>As a software engineer I can apply my excitment for continued learning, teamwork, and innovation
+                <p className="about-content">As a software engineer I can apply my excitment for continued learning, teamwork, and innovation
                     on a team that is equally excited for these things. I am eager to use my skills to build
                     and contribute toward software that empowers people and organizations.
                 </p>
-                <div className="about_technologies_container">
-                    <p>Here are a few technologies I have been working with:</p>
-                    <div className='about_technologies'>
+                <div className="about-technologies-container">
+                    <p className="about-technologies-header">Here are a few technologies I have been working with:</p>
+                    <div className='about-technologies'>
                         <p className="technology">JavaScript</p>
                         <p className="technology">React</p>
                         <p className="technology">Firebase</p>
