d36ad78e6c0c7abe1892e72c343495fb2081591e
Kaitlyn Greve <kgreve14@gmail.com>
2023-02-01 16:45:54 -0800

    styling phase 1 on header done

--- src/Header.js
+++ src/Header.js
@@ -3,7 +3,7 @@ import { useState, useRef, useEffect } from 'react';
 const subheaders = [
     'I am a Software Engineer.',
     'I build things for the web.',
-    'I am a problem solver.'
+    'I am an avid learner.'
 ]
 
 function Header() {
--- src/Nav.js
+++ src/Nav.js
@@ -2,12 +2,14 @@
 
 function Nav() {
     return (
-        <div>
+        <div className="nav-bar-container">
             <a href='#main'>KG(logo type thing)</a>
-            <a href='#about'>About</a>
-            <a href='#projects'>Work</a>
-            <a href='#blogs'>Blog</a>
-            <a href='#contact'>Contact</a>
+            <div className="nav-bar">
+                <a className="nav-item" href='#about'>About</a>
+                <a className="nav-item" href='#projects'>Work</a>
+                <a className="nav-item" href='#blogs'>Blog</a>
+                <a className="nav-item" href='#contact'>Contact</a>
+            </div>
         </div>
     )
 }
