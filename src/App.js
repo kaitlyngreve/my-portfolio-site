05ab4725a99bd6cea2a5f3b614bf75df1329f86f
Kaitlyn Greve <kgreve14@gmail.com>
2023-02-06 10:10:11 -0800

    fixed svgs

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import './App.css';
+import Nav from './Nav';
+import SideNav from './SideNav';
 import Header from './Header';
 import About from './About';
 import Projects from './Projects';
 import BlogPosts from './BlogPosts';
-import Nav from './Nav';
 import Contact from './Contact'
 
 function App() {
   return (
     <div className="App">
       <Nav />
+      <SideNav />
       <div className='whole-app-container'>
         <Header />
         <About />
---
+++ src/SideNav.js
@@ -0,0 +1,21 @@
+
+
+function SideNav() {
+    return (
+        <div className="socials-container">
+            <a className="side-item" href='https://www.linkedin.com/in/kaitlyn-greve/' target='_blank'>
+                <svg xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-linkedin"><title>LinkedIn</title><path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z"></path><rect x="2" y="9" width="4" height="12"></rect><circle cx="4" cy="4" r="2"></circle></svg>
+            </a>
+            <a className="side-item" href='https://github.com/kaitlyngreve' target='_blank'>
+                <svg xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-github"><title>GitHub</title><path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path></svg>
+            </a>
+            <a className="side-item" href='https://medium.com/@kgreve14' target='_blank'>
+                <svg stroke="currentColor" fill="currentColor" viewBox="0 0 1043.63 592.71" height="1em" width="1em">
+                    <path d="M588.67 296.36c0 163.67-131.78 296.35-294.33 296.35S0 460 0 296.36 131.78 0 294.34 0s294.33 132.69 294.33 296.36M911.56 296.36c0 154.06-65.89 279-147.17 279s-147.17-124.94-147.17-279 65.88-279 147.16-279 147.17 124.9 147.17 279M1043.63 296.36c0 138-23.17 249.94-51.76 249.94s-51.75-111.91-51.75-249.94 23.17-249.94 51.75-249.94 51.76 111.9 51.76 249.94"></path>
+                </svg>
+            </a>
+        </div>
+    )
+}
+
+export default SideNav;
