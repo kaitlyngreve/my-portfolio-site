96a82b68b1909c3ceb39e31a77945049ae50e776
Kaitlyn Greve <kgreve14@gmail.com>
2023-02-06 16:40:04 -0800

    version 1 completed

--- src/Contact.js
+++ src/Contact.js
@@ -5,8 +5,9 @@ function Contact() {
         <>
             <div className="contact-container">
                 <div id='contact'></div>
-                <p className="header">Get In Touch</p>
-                <p>I am currently searching for a role as a fullstack, frontend, or backend software engineer.
+                <p className="header-3">Get In Touch</p>
+                <div className="underline"></div>
+                <p className="contact-content">I am currently searching for a role as a fullstack, frontend, or backend software engineer.
                     If you have or know of any opportunites, definitely reach out! I look forward to hearing from you.
                 </p>
                 <a className="CTA-button email-button" href='https://mail.google.com/mail/u/0/?tab=rm&ogbl#inbox?compose=CllgCJZczqwnTflPGSbRGLHdVJQzRlXxqcBwJxgrRHTsGDgxsWMkxMMNWkNCVVrLSdsjTcJVvSV' target='_blank'>
